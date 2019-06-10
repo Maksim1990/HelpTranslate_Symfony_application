e5d97f6f47caf8e58fcc62477093442a42677d6f
Maksim Narushevich <narushevich.maksim@gmail.com>
2019-06-10 22:30:16 +0300

    no message

---
+++ deploy/mapping_docker_env_with_mysql.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+sed -e "s/\${APP_HTTP_PORT}/${DEV_HTTP_PORT}/g;
+             s/\${APP_HTTPS_PORT}/${DEV_HTTPS_PORT}/g;
+             s/\${APP_REDIS_PORT}/${DEV_REDIS_PORT}/g;
+             s/\${APP_MYSQL_PORT}/${DEV_MYSQL_PORT}/g;
+             s/\${APP_MYSQL_PASSWORD}/${DEV_MYSQL_PASSWORD}/g;
+             s/\${APP_MYSQL_DATABASE}/${DEV_MYSQL_DATABASE}/g;"  ./deploy/docker-compose.dev.tpl.yml > ./docker-compose.yml
+sed -e "s/\${APP_MYSQL_PASSWORD}/${DEV_MYSQL_PASSWORD}/g;
+ s/\${APP_MYSQL_DATABASE}/${DEV_MYSQL_DATABASE}/g;"  ./deploy/.env.dist.deploy > ./deploy/.env.dist
