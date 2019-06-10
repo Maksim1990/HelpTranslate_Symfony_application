6a5e1d616f46a2826c61c539fb77be942216ef4b
Maksim Narushevich <narushevich.maksim@gmail.com>
2019-06-10 22:51:50 +0300

    no message

--- deploy/mapping_environment_credentials_and_ports.sh
+++ deploy/mapping_environment_credentials_and_ports.sh
@@ -2,8 +2,8 @@
 echo $1
 if [ $1 == "yes" ]; then
     echo "Start App environment WITH Shared Mysql service";
-    #./deploy/mapping_docker_env_without_mysql.sh
+    ./deploy/mapping_docker_env_without_mysql.sh
 else
   echo "Start App environment WITHOUT Shared Mysql service";
-  #./deploy/mapping_docker_env_with_mysql.sh
+  ./deploy/mapping_docker_env_with_mysql.sh
 fi
