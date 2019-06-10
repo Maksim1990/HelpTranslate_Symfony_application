7b907d22c69557534511e4eb4d3813169a55897b
Maksim Narushevich <narushevich.maksim@gmail.com>
2019-06-11 00:38:08 +0300

    no message

--- deploy/mapping_symfony_db_credentials.sh
+++ deploy/mapping_symfony_db_credentials.sh
@@ -2,7 +2,7 @@
 
 if [ $1 == "yes" ]; then
 echo "Set DB connection with SHARED MySQL container";
-    sed -e "s/\${APP_MYSQL_PORT}/${DEV_MYSQL_PORT}/g;
+    sed -e "s/\${APP_MYSQL_PORT}/${DEV_SHARED_MYSQL_PORT}/g;
              s/\${APP_MYSQL_PASSWORD}/${DEV_SHARED_MYSQL_PASSWORD}/g;
              s/\${APP_MYSQL_DATABASE}/${DEV_MYSQL_DATABASE}/g;"  ./app/config/parameters.yml.deploy > ./app/config/parameters.yml
 else
